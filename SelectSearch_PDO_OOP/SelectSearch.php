7c827b5ea745293c046bfdd9db5fd809d0fc6a7d
Mohammed Khusro <makhusro@hotmail.com>
2014-08-14 12:31:12 -0400

    code samples-select search using pdo oop

---
+++ SelectSearch_PDO_OOP/SelectSearch.php
@@ -0,0 +1,35 @@
+<?php 
+include('searchuser.inc');
+$obj=new SearchUser();
+$res=$obj->SelAll();
+foreach ($res as $row)
+{
+    echo $row['id']." | ".$row['username']." | ".$row['email']."<br>";
+}
+echo "<br>";
+?>
+<form  action="test.php" method="POST">
+  <select name="id" onchange="this.form.submit()">
+	  <option value="">select a value</option>
+    <?php foreach ($res as $row) { ?>
+      <option value="<?php echo $row['id']; ?>"><?php echo $row['username']; ?></option>
+    <?php } ?>
+  </select>
+  </form>
+<?php 
+if (isset($_POST)!="")
+{
+	$id =$_POST['id'];
+	$resid=$obj->searchID($id);
+	echo $resid->id." | ". $resid->username." | ".$resid->email; 
+} 
+?>				
+	  
+	  	  
+
+
+
+
+
+               
+          
